a41d5571964e9ea8fc739889f00e55da38cb5474
wiyochi <mat.arquilliere@gmail.com>
2019-11-25 12:00:54 +0100

    Ajout de l'XML pour la sidebar de navigation -> pas de kotlin fonctionnel donc ne compile pas

--- Application/app/src/main/java/fr/isima/az/application/MainActivity.kt
+++ Application/app/src/main/java/fr/isima/az/application/MainActivity.kt
@@ -2,11 +2,21 @@ package fr.isima.az.application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.navigation.NavigationView
+import android.widget.Toolbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    val toolbar: Toolbar =
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //FOR DESIGN
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
