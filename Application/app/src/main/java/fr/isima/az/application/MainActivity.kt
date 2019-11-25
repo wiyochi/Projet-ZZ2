43c68f62c062824242a9d1a43014b851a43b9696
wiyochi <mat.arquilliere@gmail.com>
2019-11-25 17:18:00 +0100

    ajout du code kotlin du menu de navigation -> ne fonctionne pas: crash (a determiner)

--- Application/app/src/main/java/fr/isima/az/application/MainActivity.kt
+++ Application/app/src/main/java/fr/isima/az/application/MainActivity.kt
@@ -2,21 +2,66 @@ package fr.isima.az.application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.navigation.NavigationView
-import android.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    val toolbar: Toolbar =
+    private var toolbar: Toolbar? = null
+    private var drawerLayout : DrawerLayout? = null
+    private var navigationView : NavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*configureToolbar()
+        configureDrawerLayout()
+        configureNavigationView()*/
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout!!.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout?.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+
+        /*
+        val id = item.getItemId()
+
+        when(id) {
+            R.id.activity_new_travel ->
+            R.id.activity_stats ->
+            R.id.settings_activity ->
+        }
+        */
+        this.drawerLayout?.closeDrawer(GravityCompat.START)
+
+        return true
+    }
+
+    private fun configureToolbar() {
+        toolbar = findViewById(R.id.activity_main_toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    private fun configureDrawerLayout() {
+        drawerLayout = findViewById(R.id.activity_main_drawer_layout)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    private fun configureNavigationView() {
+        navigationView = findViewById(R.id.activity_main_nav_view)
+        navigationView?.setNavigationItemSelectedListener(this)
     }
 }
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    //FOR DESIGN
-    private Toolbar toolbar;
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
