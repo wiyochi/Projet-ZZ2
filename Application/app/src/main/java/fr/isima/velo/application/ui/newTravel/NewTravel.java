a307c5927cdbcf25d5ffcdae4d33881ee8b1abc0
wiyochi <mat.arquilliere@gmail.com>
2020-01-21 15:56:58 +0100

    On peut obtenir des points GPS latitude-longitude (affichés dans un textView pour debug

--- Application/app/src/main/java/fr/isima/velo/application/ui/newTravel/NewTravel.java
+++ Application/app/src/main/java/fr/isima/velo/application/ui/newTravel/NewTravel.java
@@ -1,7 +1,16 @@
 package fr.isima.velo.application.ui.newTravel;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,11 +20,22 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import fr.isima.velo.application.R;
+
+import static java.lang.System.*;
 
 public class NewTravel extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Button newTravelButton;
+    private TextView debugTextView;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +45,84 @@ public class NewTravel extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        newTravelButton = findViewById(R.id.button_new_travel);
+        debugTextView = findViewById(R.id.debugCoords);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                debugTextView.append("\n latitude: " + location.getLatitude() + "longitude: " + location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 10);
+            return;
+        }
+        else {
+            configureLocationManager();
+        }
+
+        configureButton();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureLocationManager();
+                return;
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void configureLocationManager() {
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        String provider = locationManager.getBestProvider(criteria, true);
+        if(provider != null) {
+            locationManager.requestLocationUpdates(provider, 10, 10, locationListener);
+            debugTextView.append(provider);
+        } else {
+            locationManager.requestLocationUpdates("gps", 10, 10, locationListener);
+        }
+    }
+
+    private void configureButton() {
+
+        newTravelButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View v) {
+                debugTextView.append("\n\nlatitude: " + locationManager.getLastKnownLocation("gps").getLatitude() + "\nlongitude: " + locationManager.getLastKnownLocation("gps").getLongitude());
+            }
+        });
+    }
 
     /**
      * Manipulates the map once available.
