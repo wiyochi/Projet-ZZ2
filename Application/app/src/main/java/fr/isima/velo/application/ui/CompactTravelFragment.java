c31b4f4b3e00aa3b619911bcea8a4eea612b9b6b
wiyochi <mat.arquilliere@gmail.com>
2020-02-06 17:45:35 +0100

    ajout d'un historique basique

--- Application/app/src/main/java/fr/isima/velo/application/ui/CompactTravelFragment.java
+++ Application/app/src/main/java/fr/isima/velo/application/ui/CompactTravelFragment.java
@@ -4,18 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
 import fr.isima.velo.application.R;
+import fr.velo.lib.Journey;
 
 public class CompactTravelFragment extends Fragment {
+
+    private Journey journey;
+    private TextView name;
+    private TextView date;
+    private TextView time;
+
+    public CompactTravelFragment(Journey j) {
+        journey = j;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View root = inflater.inflate(R.layout.fragment_compact_travel, container, false);
 
+        name = root.findViewById(R.id.compact_travel_title);
+        date = root.findViewById(R.id.compact_travel_date);
+        time = root.findViewById(R.id.compact_travel_duration);
+
+        name.setText(journey.getName());
+        date.setText("" + journey.getDateTime());
+        time.setText("none");
 
         return root;
     }
--- Application/app/src/main/java/fr/isima/velo/application/ui/history/HistoryFragment.java
+++ Application/app/src/main/java/fr/isima/velo/application/ui/history/HistoryFragment.java
@@ -10,31 +10,31 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import java.util.Iterator;
+
 import fr.isima.velo.application.R;
 import fr.isima.velo.application.ui.CompactTravelFragment;
 import fr.isima.velo.application.ui.newTravel.MapNewTravel;
+import fr.velo.lib.Journey;
+import fr.velo.lib.JourneyHistory;
 
 public class HistoryFragment extends Fragment {
-
-    private CompactTravelFragment frag1;
-    private CompactTravelFragment frag2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View root = inflater.inflate(R.layout.fragment_history, container, false);
 
-
-        frag1 = new CompactTravelFragment();
-        frag2 = new CompactTravelFragment();
-
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        fragmentTransaction.add(R.id.container_history, frag1, "one");
-        fragmentTransaction.add(R.id.container_history, frag2, "one");
-        fragmentTransaction.commit();
+        Iterator<Journey> it = JourneyHistory.getInstance().iterator();
+        while (it.hasNext()) {
+            CompactTravelFragment c = new CompactTravelFragment(it.next());
+            fragmentTransaction.add(R.id.container_history, c, "one");
+        }
 
+        fragmentTransaction.commit();
 
         return root;
     }
